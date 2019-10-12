4c4da3522a6f13d2bcf23d7a3a8f7b7c3751b51c
Kalle Rustholkarhu <kalle.rustholkarhu@gmail.com>
2019-10-12 14:24:38 +0300

    Work okay

--- remote_beep.py
+++ remote_beep.py
@@ -4,11 +4,8 @@ from ev3dev2.sound import Sound
 from ev3dev2.sensor.lego import InfraredSensor
 from time import sleep
 
-# Steer pair
-
-lm = LargeMotor(OUTPUT_B)
-rm = LargeMotor(OUTPUT_C)
-steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
+# Tank pair
+tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
 
 # Infrared remote
 ir = InfraredSensor()
@@ -18,34 +15,16 @@ sound = Sound()
 # Play a standard beep on boot up
 sound.beep()
 
-def top_left_channel_1_action():
-    steer_pair.on_for_seconds(steering=0, speed=50, seconds=1)
-
-def top_right_channel_1_action():
-    steer_pair.on_for_seconds(steering=100, speed=50, seconds=1)
-
-def bottom_right_channel_1_action():
-    steer_pair.on_for_seconds(steering=0, speed=-50, seconds=1)
-
-def bottom_left_channel_1_action():
-    steer_pair.on_for_seconds(steering=-100, speed=50, seconds=1)
-
-    
-ir.on_channel1_top_right = top_right_channel_1_action
-ir.on_channel1_top_left = top_left_channel_1_action
-ir.on_channel1_bottom_right = bottom_right_channel_1_action
-ir.on_channel1_bottom_left = bottom_left_channel_1_action
-
 while True:
     button_code = ir.value()
     if button_code == ir.TOP_LEFT:
-        steer_pair.on(75, 75)
+        tank_pair.on(left_speed=50, right_speed=50)
     elif button_code == ir.TOP_RIGHT:
-        steer_pair.on(-75, 75)
+        tank_pair.on(left_speed=-75, right_speed=75)
     elif button_code == ir.BOTTOM_RIGHT:
-        steer_pair.on(-75,-75)
+        tank_pair.on(left_speed=-75, right_speed=-75)
     elif button_code == ir.BOTTOM_LEFT:
-        steer_pair.on(75, -75)
+        tank_pair.on(left_speed=75, right_speed=-75)
     else:
-        steer_pair.off(brake=True)
+        tank_pair.off()
     sleep(0.01)
