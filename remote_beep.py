1d0d30733f9c8caba50e7674d3897b0893d4b1bf
Kalle Rustholkarhu <kalle.rustholkarhu@gmail.com>
2019-10-12 13:26:04 +0300

    Rumble

--- remote_beep.py
+++ remote_beep.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor, MoveSteering, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sound import Sound
 from ev3dev2.sensor.lego import InfraredSensor
 from time import sleep
+
+# Steer pair
+
+lm = LargeMotor(OUTPUT_B)
+rm = LargeMotor(OUTPUT_C)
+steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
 
 # Infrared remote
 ir = InfraredSensor()
 
 sound = Sound()
-#play a standard beep
+# Play a standard beep on boot up
 sound.beep()
 
 def top_left_channel_1_action(state):
-    print("top left on channel 1: %s" % state)
+    steer_pair.on_for_seconds(steering=0, speed=50, seconds=1)
+
+def top_right_channel_1_action(state):
+    steer_pair.on_for_seconds(steering=100, speed=50, seconds=1)
 
 def bottom_right_channel_1_action(state):
-    print("bottom right on channel 1: %s" % state)
+    steer_pair.on_for_seconds(steering=0, speed=-50, seconds=1)
 
-ir = InfraredSensor()
+def bottom_left_channel_1_action(state):
+    steer_pair.on_for_seconds(steering=-100, speed=50, seconds=1)
+
+    
+ir.on_channel1_top_right = top_right_channel_1_action
 ir.on_channel1_top_left = top_left_channel_1_action
 ir.on_channel1_bottom_right = bottom_right_channel_1_action
+ir.on_channel1_bottom_left = bottom_left_channel_1_action
 
 while True:
     ir.process()
