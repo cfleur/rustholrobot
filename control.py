299f6c0eaeb9e30e58b811da4758d77196bafbd7
Kalle Rustholkarhu <kalle.rustholkarhu@gmail.com>
2019-10-13 10:20:14 +0300

    Combined moving and grabbing

---
+++ control.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.sound import Sound
+from ev3dev2.sensor.lego import InfraredSensor
+from time import sleep
+
+
+# Grabber arm
+mm = MediumMotor('outA')
+
+# Tank pair
+tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
+
+# Infrared remot
+ir = InfraredSensor()
+ir.mode = ir.MODE_IR_REMOTE
+
+sound = Sound()
+# Play a standard beep on boot up
+sound.beep()
+
+while True:
+    button_code = ir.value()
+    if button_code == ir.TOP_LEFT:
+        tank_pair.on(left_speed=100, right_speed=100)
+    elif button_code == ir.TOP_LEFT_BOTTOM_LEFT:
+        tank_pair.on(left_speed=100, right_speed=0)
+    elif button_code == ir.TOP_LEFT_TOP_RIGHT:
+        tank_pair.on(left_speed=0, right_speed=100)
+    elif button_code == ir.TOP_RIGHT:
+        tank_pair.on(left_speed=-100, right_speed=100)
+    elif button_code == ir.BOTTOM_RIGHT:
+        tank_pair.on(left_speed=-100, right_speed=-100)
+    elif button_code == ir.BOTTOM_LEFT:
+        tank_pair.on(left_speed=100, right_speed=-100)
+    elif button_code == ir.BOTTOM_LEFT_BOTTOM_RIGHT:
+        tank_pair.on(left_speed=0, right_speed=-100)
+    elif button_code == ir.TOP_RIGHT_BOTTOM_RIGHT:
+        tank_pair.on(left_speed=-100, right_speed=0)
+    elif button_code == ir.TOP_LEFT_BOTTOM_RIGHT:
+        mm.run_timed(time_sp=5, speed_sp=1000)
+    elif button_code == ir.TOP_RIGHT_BOTTOM_LEFT:
+        mm.run_timed(time_sp=5, speed_sp=-1000)
+    else:
+        tank_pair.off()
+    sleep(0.01)
